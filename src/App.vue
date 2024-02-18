c8d727c519b12b0424f8fa53209b258edcbe0c4e
okane <okanekudasai6@gmail.com>
2024-02-19 01:03:28 +0900

    addENV

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <template lang="">
     <div>
         <div>랜덤 채팅</div>
-        
     </div>
 </template>
 <script>
 export default {
-
+    created() {
+        console.log(import.meta.env.VITE_API_SERVER);
+    },
 }
 </script>
 <style lang="">
