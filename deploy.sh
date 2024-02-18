07376f29e41e922e5f94b6ebd26a0c15b011d406
okane <okanekudasai6@gmail.com>
2024-02-18 23:31:30 +0900

    testAction

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+sudo mv -f /home/ubuntu/deploy_front/favicon.ico /usr/share/nginx/html/
+sudo mv -f /home/ubuntu/deploy_front/index.html /usr/share/nginx/html/
+sudo mv -f /home/ubuntu/deploy_front/assets /usr/share/nginx/html/
