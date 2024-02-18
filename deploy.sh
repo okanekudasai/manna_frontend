8e775733dfb77dd4b450bbab917683a04be3d42f
okane <okanekudasai6@gmail.com>
2024-02-18 23:35:03 +0900

    testAction

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 sudo mv -f /home/ubuntu/deploy_front/favicon.ico /usr/share/nginx/html/
 sudo mv -f /home/ubuntu/deploy_front/index.html /usr/share/nginx/html/
-sudo mv -f /home/ubuntu/deploy_front/assets /usr/share/nginx/html/
+rsync -a /home/ubuntu/deploy_front/assets/ /usr/share/nginx/html/
+rm -rf /home/ubuntu/deploy_front/assets/
