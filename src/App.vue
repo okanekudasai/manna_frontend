6eafc3693840b51a1bae5f8b4c77fb14fc29c4c5
okane <okanekudasai6@gmail.com>
2024-02-19 00:30:40 +0900

    faviconEdit

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,10 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!@@@@@@@" />
+      <HelloWorld msg="You did it!" />
 
       <nav>
-        <RouterLink to="/">Hom!!!!!!!!e</RouterLink>
+        <RouterLink to="/">Hom!e</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
