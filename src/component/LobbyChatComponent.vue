39ff27baab1b3c5e5468f96732a5020b3769e912
okane <okanekudasai6@gmail.com>
2024-05-16 00:48:55 +0900

    init

--- src/component/LobbyChatComponent.vue
+++ src/component/LobbyChatComponent.vue
@@ -91,7 +91,8 @@ export default {
             console.log("바뀜!!!");
             let viewportHeight = window.visualViewport.height; // viewport 의 높이
             console.log("화면높이 " + viewportHeight);
-            useVhStore().vh = viewportHeight + "px";
+            useVhStore().vh = (viewportHeight * 0.01) + "px";
+            console.log(useVhStore().vh);
         },
         close_modal() {
             this.$emit('close_lobby_chat_modal');
