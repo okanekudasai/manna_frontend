43be8183d8246949d10249fa26c5675e10df66a4
okane <okanekudasai6@gmail.com>
2024-05-15 23:17:01 +0900

    keyboardLaodingTimeAdd

--- src/component/LobbyChatComponent.vue
+++ src/component/LobbyChatComponent.vue
@@ -98,12 +98,12 @@ export default {
                 useVhStore().vh = window.visualViewport.height * 0.01 + 'px';
                 document.querySelector("body").style.height = window.visualViewport.height + 'px';
                 document.querySelector("body").style.overflow = "hidden"
-            }, 5000)
+            }, 1000)
         },
         setVhOut() {
             setTimeout(() => {
                 useVhStore().vh = window.visualViewport.height * 0.01 + 'px';
-            }, 5000)
+            }, 1000)
         }
     }
 }
